6a2c2b0cc4ac70d2de2145a6c3fae0a0c9423aa3
ly1010 <liyang372328@163.com>
2024-05-07 00:15:19 +0800

    创建web服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-const greeting = 'Come!'
-console.log(greeting)
+const http = require('http')
+const server = http.createServer((request, response) =>{
+    response.write('hello')
+    response.end()
+})
+
+server.listen(3000, () => {
+    console.log('🚀服务已启动....')
+})
